0fa3f3ea435d4ce4a954495b51937d3a75ab8987
RAZASHAH009 <officialraza262@gmail.com>
2024-03-05 20:38:47 +0500

    update

---
+++ app.js
@@ -0,0 +1,177 @@
+//Chapter 4
+
+//Q1
+var a = "raza", b = "laraib",  c = "hadid"
+
+//Q2
+
+//legal
+ 
+// myvar
+// camelcase
+// a_variable
+// fullName
+// another123
+
+//illegal
+
+// 123xyz
+// abc-xyz
+// abc xyz
+// abc.xyz
+// abc@xyz
+
+// Q3
+
+// a
+var a = (" Rules for naming JS variable ")
+document.write("<h1>"+a+ "</h1>")
+//b
+document.write("<p>variable name can only contain, number $ and _ . For example : $my_1st Variable  </p>")
+//c
+document.write("<p>variable must begin with a letter, $ or _ .For example : $name,_name or name </p>")
+//d
+document.write("<p>variable name are case sensitive</p>")
+//e
+document.write("<p>variable name should not be JS keyboards  </p>")
+
+//Chapter 5
+
+//Q1
+
+var num1 = 3;
+var num2 = 5;
+                //plus
+document.write( "sum of 3 and 5 is" ,+(num1+num2)+ "<br>")
+
+//Q2
+                //subtration
+document.write("subtration of 5 and 3 is" ,+(num1-num2)+"<br>" )
+                //multiplication
+document.write( "multiplication of 3 and 5 is", +(num1*num2)+"<br>") 
+              //division
+document.write( "division of 3 and 5 is", +(num1/num2)+"<br>") 
+              // modulus
+document.write( "sum of 10 and 5 is", +(num1%num2)+"<br>")             
+
+//Q3
+
+var my 
+
+document.write("value after variable declaration is:." + my + "<br>" )
+
+var myy = 4;
+
+document.write ("initial value"+myy)
+
+
+//Q4
+ var myyy = 600
+ var myyyy = 5
+ document.write("Total cost to buy 5 ticket to a movie is "+(600*5)+ "PKR <br>" )
+
+ //Q5
+  
+ var a = 4 
+ var b =1
+ document.write("<h1>Table of for</h1>")
+ document.write("4x1="+(a)+"<br>" )
+ document.write("4x2="+(a*++b)+"<br>" )
+ document.write("4x3="+(a*++b)+"<br>" )
+ document.write("4x4="+(a*++b)+"<br>" )
+ document.write("4x5="+(a*++b)+"<br>" )
+ document.write("4x6="+(a*++b)+"<br>" )
+ document.write("4x7="+(a*++b)+"<br>" )
+ document.write("4x8="+(a*++b)+"<br>" )
+ document.write("4x9="+(a*++b)+"<br>" )
+ document.write("4x10="+(a*++b)+"<br><br>" )
+
+ //Q6
+
+ //Q7
+
+ var zp1 = 650
+ var yq = 3
+ var zp2 = 100
+ var yq2 = 7
+ var shiping = 100
+ var result=(zp1*yq)
+ var result2=(zp2*yq2)
+ var total = (result+result2+shiping)
+
+ document.write("Price of item 1 is 650<br>","Ordered quantity of item 1is 3 <br>","Price of item 2 is 100<br>",
+ "Ordered Quantity of item 2 is 7<br> ","Shipping charges  100 <br><br><br>" )
+
+document.write("Total cost of your order"+total)
+
+// Q8
+
+var full = 980
+var Name = 804
+var result3 = (Name/full)*100
+
+
+document.write("<h1>Marks Sheet</h1>")
+document.write("Total Marks 980 <br>")
+document.write("Marks obtained 804 <br> " )
+document.write("percentage"+ result3 + "% <br><br><br>")
+
+//Q10 
+var fulll = 10
+console.log((fulll + 5) * 10)/ 2+"br> ";
+
+
+//Q11 
+
+var w22 = 2024;
+var w222 = 2005;
+
+var w2222 = (2024-2005)
+
+document.write("current year: "+w22+"<br>" )
+document.write("birthday year: " +w222+"<br>")
+document.write("your Age: " +w2222+"<br><br>")
+
+
+
+//  chapter 6 
+
+
+// Q1
+ var aaa = 10;
+
+ document.write(aaa+"<br>")
+ document.write(++aaa+"<br>")
+ document.write(++aaa+"<br>")
+ document.write(--aaa+"<br>")
+
+
+//  Q2 
+
+var bb = 3;
+var cc = 4;
+document.write("<h1></h1>")
+console.log (--bb - --cc + ++bb + bb--);
+
+     document.write(2-3+3+3+"<br>");
+
+
+    //  Q3
+    // Q4
+    // Q5
+    // Q6
+    
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
